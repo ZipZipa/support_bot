2f87c151abc1cc6543137d30d5899e5d069db810
vivivixmc <penekmail@gmail.com>
2022-05-26 21:58:05 +0500

    добавлена функция создания и добавления кнопки в дб через юниксвремя

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from loader import dp
 
 import handlers
 
-from utils.db.db_menu import menu3, addQuestion
+from utils.db.db_menu import menu3, add_question
 
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -21,6 +21,6 @@ async def on_startup(dispatcher):
 
 if __name__ == '__main__':
     print(menu3())
-    addQuestion(100,"test",0)
+    #add_question(100,"test",0)
     print(menu3())
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
