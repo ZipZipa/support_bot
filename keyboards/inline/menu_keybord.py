abeadad1c626bd2ff47f22978d68b87fcf802e42
enborisov <enborisov@sber.ru>
2022-06-02 12:56:51 +0300

    pep8 menu_keybord

--- keyboards/inline/menu_keybord.py
+++ keyboards/inline/menu_keybord.py
@@ -1,26 +1,38 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.callback_data import CallbackData
-from handlers.users.admin import check_admin
-from utils.db.db_menu import get_stage1
 import logging
 
-# Создаем CallbackData-объекты, которые будут нужны для работы с менюшкой и вызова создания кнопки
-menu_cd = CallbackData("menu", "next_level", "button_name", "button_rezult", "test_pre_level")
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.callback_data import CallbackData
+
+from handlers.users.admin import check_admin
+
+from utils.db.db_menu import get_stage1
+
+
+# Создаем CallbackData-объекты, которые будут нужны
+# для работы с менюшкой и вызова создания кнопки
+menu_cd = CallbackData("menu", "next_level", "button_name",
+                       "button_rezult", "test_pre_level")
 
 cbd_admin = CallbackData("add_btn", "pre_level", "level")
 
 
 # С помощью этой функции будем формировать коллбек дату для каждого
 # элемента меню,в зависимости от переданных параметров.
-def make_callback_data(next_level, button_name="0", button_rezult="0", test_pre_level="0"):
-    return menu_cd.new(next_level=next_level, button_name=button_name, button_rezult=button_rezult, test_pre_level=test_pre_level)
+def make_callback_data(next_level, button_name="0", button_rezult="0",
+                       test_pre_level="0"):
+    return menu_cd.new(next_level=next_level,
+                       button_name=button_name,
+                       button_rezult=button_rezult,
+                       test_pre_level=test_pre_level)
+
 
 def make_new_button_data(pre_level="0", level="0"):
     return cbd_admin.new(pre_level=pre_level, level=level)
 
 
 # Создаем функцию, которая отдает клавиатуру с доступными категориями выбора
-async def categories_keyboard(current_level, sql, user_id, test_pre_level, next_level):
+async def categories_keyboard(current_level, sql, user_id,
+                              test_pre_level, next_level):
     markup = InlineKeyboardMarkup(row_width=2)
     logging.info('Function categories_keyboard')
     categories = get_stage1(sql)
@@ -38,12 +50,16 @@ async def categories_keyboard(current_level, sql, user_id, test_pre_level, next_
         # Сформируем колбек дату, которая будет на кнопке.
         callback_data = make_callback_data(
             button_name=category[1],        # Название кнопки
-            button_rezult=category[2],      # Проверка на тип кнопки 0-переход к следующей кнопке 1-финальный вывод 2-вывод текса и переход дальше 
+            button_rezult=category[2],      # Проверка на тип кнопки:
+                                            # 0 - переход к следующей кнопке
+                                            # 1 - финальный вывод
+                                            # 2 - вывод текса и переход дальше
             test_pre_level=category[3],     # Текущий уровень
             next_level=category[4],         # Следующий уровень
             )
 
-        logging.info(f'Prepared callback_data for button. callback_data = {callback_data}')
+        logging.info('Prepared callback_data for button. '
+                     f'callback_data = {callback_data}')
 
         # Вставляем кнопку в клавиатуру
         markup.insert(InlineKeyboardButton(
