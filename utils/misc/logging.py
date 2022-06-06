a2e9585715bda1fa71d4add315753fe531952745
enborisov <enborisov@sber.ru>
2022-06-06 17:57:35 +0300

    Логи записываются в папку ./data/logs Разбивка по дням

--- utils/misc/logging.py
+++ utils/misc/logging.py
@@ -1,6 +1,19 @@
 import logging
+from datetime import date
 
-logging.basicConfig(format=u'[%(levelname)-4s] [%(asctime)s] [LINE:%(lineno)-3s] [%(filename)-15s] %(message)s ',
+
+log_format = (u'[%(levelname)-4s] [%(asctime)s] [LINE:%(lineno)-3s] '
+              u'[%(filename)-15s] %(message)s')
+
+curr_date = date.today().strftime(r"%d-%m-%Y")
+
+logfile = (r'C:\Users\19893422\Documents\support_bot\data\logs\dops-'
+           f'{curr_date}.log')
+
+logging.basicConfig(format=log_format,
                     level=logging.INFO,
-                    # level=logging.DEBUG,  # Можно заменить на другой уровень логгирования.
+                    filename=logfile,
+                    filemode='a',
+                    # level=logging.DEBUG,
+                    # Можно заменить на другой уровень логгирования.
                     )
