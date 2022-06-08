03ca7abc28f2d4e180dd9516f38c9947c1bc4554
boriseus <borisoff.en@gmail.com>
2022-06-08 09:16:54 +0300

    fixed folder not created fixed encoding

--- utils/misc/logging.py
+++ utils/misc/logging.py
@@ -7,13 +7,14 @@ log_format = (u'[%(levelname)-4s] [%(asctime)s] [LINE:%(lineno)-3s] '
 
 curr_date = date.today().strftime(r"%d-%m-%Y")
 
-logfile = (r'C:\Users\19893422\Documents\support_bot\data\logs\dops-'
+logfile = (r'data\logs\dops-'
            f'{curr_date}.log')
 
 logging.basicConfig(format=log_format,
                     level=logging.INFO,
                     filename=logfile,
                     filemode='a',
+                    encoding='utf-8'
                     # level=logging.DEBUG,
                     # Можно заменить на другой уровень логгирования.
                     )
