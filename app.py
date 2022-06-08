574d3a5b079fe1dc2127bf188e09d4c54fd830fb
vivivixmc <penekmail@gmail.com>
2022-06-08 23:13:14 +0500

    Сделал рефакторинг кода =)

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from loader import dp
 
 import handlers
 
-from utils.db.db_menu import draw_tree, draw_tree2
+from utils.db.db_menu import draw_tree, make_tree, delete_button
 
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -18,8 +18,9 @@ async def on_startup(dispatcher):
     # Уведомляет про запуск
     # await on_startup_notify(dispatcher)
 
-    print(draw_tree())
-    print(draw_tree2(5))
-    print(draw_tree())
+    print(draw_tree(make_tree()))
+    delete_button(69)
+    print(draw_tree(make_tree()))
+
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
