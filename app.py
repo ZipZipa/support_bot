9ea41caa7cbd3224f91419351dcfe49659f3f60c
boriseus <92451139+boriseus@users.noreply.github.com>
2022-06-09 11:10:29 +0300

    Update app.py
    app.py from dev

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from loader import dp
 
 import handlers
 
-from utils.db.db_menu import draw_tree, create_tree, delete_button, show_delete
+from utils.db.db_menu import menu3
 
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -16,13 +16,7 @@ async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
 
     # Уведомляет про запуск
-    # await on_startup_notify(dispatcher)
-
-    print(draw_tree(create_tree()))
-    # print("Удалится :")
-    # print(draw_tree(show_delete(2)))
-    # delete_button(2)
-    print(draw_tree(create_tree()))
+    await on_startup_notify(dispatcher)
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
