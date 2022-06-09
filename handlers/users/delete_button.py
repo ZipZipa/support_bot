9ee72e6557b471d515f2c97f6245a62ce8139352
boriseus <borisoff.en@gmail.com>
2022-06-09 09:39:36 +0300

    logging enabled, tree is shown

--- handlers/users/delete_button.py
+++ handlers/users/delete_button.py
@@ -7,14 +7,14 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import InlineKeyboardButton as IKButton
 from aiogram.types import InlineKeyboardMarkup as IKMarkup
 
-from handlers.users.menu import show_menu
 from handlers.users.button_builder import cancel_kb
+from handlers.users.menu import show_menu
 
 from keyboards.inline.menu_keybord import cbd_delete
 
 from loader import dp
 
-from utils.db.db_menu import get_stage1, delete_button
+from utils.db.db_menu import get_stage1, delete_button, draw_tree, show_delete
 
 
 class FSMDelete(StatesGroup):
@@ -59,9 +59,10 @@ async def del_button_start(callback: types.CallbackQuery,
 async def pick_deletion(callback: types.CallbackQuery, state: FSMContext):
     async with state.proxy() as data:
         data['remove_id'] = int(callback.data.split('_')[-1])
+        list_deletion = draw_tree(show_delete(data['remove_id']))
     await FSMDelete.next()
-    await callback.message.answer('Чтобы удалить выбранную кнопку '
-                                  'и все содержимое, введите '
+    await callback.message.answer(f'Будет удалено:\n{list_deletion} '
+                                  '\nДля удаления введите: '
                                   '"подтверждаю".',
                                   reply_markup=cancel_kb)
 
@@ -71,6 +72,8 @@ async def confirmation(message: types.Message, state: FSMContext):
     if message.text.lower() == "подтверждаю":
         async with state.proxy() as data:
             delete_button(data['remove_id'])
+            logging.info(f'{message.from_user.id} удалил main_pages '
+                         f'_id={data["remove_id"]} и её взаимосвязи')
         await message.answer('Кнопка удалена!')
         await state.finish()
         await show_menu(message)
