f7818d879b70e8ce73aa74c225fd63fdf79e68d2
Artem <gor4eev@mail.ru>
2022-06-12 03:11:47 +0300

    Update app.py
    Убран тестовый принт

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from loader import dp
 
 import handlers
 
-from utils.db.db_menu import draw_tree, create_tree
-
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
@@ -18,7 +16,6 @@ async def on_startup(dispatcher):
 
     # Уведомляет про запуск
     # await on_startup_notify(dispatcher)
-    print(draw_tree(create_tree()))
 
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
