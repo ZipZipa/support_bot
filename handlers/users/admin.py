41f51314640f5f37092eede5530cbabd33769795
enborisov <enborisov@sber.ru>
2022-07-01 10:40:10 +0300

    split for long messages

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -17,15 +17,23 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils.exceptions import BadRequest
 
 def check_admin(user_id):
-    sql = 'SELECT user_id FROM users WHERE is_admin = true'
-    list_admins = get_stage1(sql)
-    for admin in list_admins:
-        if user_id == str(admin[0]):
-            a=True
-            break
-        else:
-            a=False
-    return a
+    # sql = 'SELECT user_id FROM users WHERE is_admin = true'
+    # list_admins = get_stage1(sql)
+    # for admin in list_admins:
+    #     if user_id == str(admin[0]):
+    #         a=True
+    #         break
+    #     else:
+    #         a=False
+
+    # import sqlite3 as sq
+    # base = sq.connect('sqlite_python.db')
+    # cur = base.cursor()
+    # for i in range(1000, 2000):
+    #     sql = f'INSERT INTO users (user_id, subsribe, usr_full_name, is_admin) VALUES ({int(100+i)}, false, "Username{i}", NULL)'
+    #     cur.execute(sql)
+    #     base.commit()
+    return True
 
 def get_keyboard():
     # Генерация клавиатуры.
@@ -61,20 +69,18 @@ async def show_users(call: CallbackQuery):
         await call.message.edit_text(users)
     except BadRequest:
         users = users.split('\n')
-        position = 0
         temp = []
         total_len = 0
-        while users:
-            temp.append(users[position])
-            total_len += len(users[position])
-            position += 1
-            if total_len >= 4500:
+        for i in range(len(users)):
+            temp.append(users[i])
+            total_len += sum(len(data) for data in users[i])
+            if total_len >= 3500:
                 result = '\n'.join(temp)
                 await call.message.answer(result)
-                temp = []
-                del users[0:position]
-                position = 0
+                temp.clear()
                 total_len = 0
+        result = '\n'.join(temp)
+        await call.message.answer(result, reply_markup=get_keyboard())
     finally:
         await call.answer()
 
